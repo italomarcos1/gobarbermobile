c5aa42a5508d3191521d266b3b6394ed175ee1d2
Italo M <italomarcos0010@gmail.com>
2020-03-27 20:51:01 -0300

    done -- routes/tab navigator, provider/useSelector, app

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import SignIn from '~/pages/SignIn';
 import SignUp from '~/pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
 
-const routes = createAppContainer(createSwitchNavigator({ SignIn, SignUp }));
-
-export default routes;
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({ SignIn, SignUp }),
+        App: createBottomTabNavigator({ Dashboard }),
+      },
+      { initialRouteName: isSigned ? 'App' : 'Sign' }
+    )
+  );
