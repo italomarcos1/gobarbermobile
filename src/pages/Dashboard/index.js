7ff3d1c0775ca72face89e2846abb606cba0c725
Italo M <italomarcos0010@gmail.com>
2020-03-28 14:23:53 -0300

    appointments on dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
-// import { Container } from './styles';
+import Appointment from '~/components/Appointment';
+
+import api from '~/services/api';
+import { Container, List, Title } from './styles';
 
 export default function Dashboard() {
-  return <Background />;
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map(apnt =>
+        apnt.id === id
+          ? { ...apnt, canceled_at: response.data.canceled_at }
+          : apnt
+      )
+    );
+
+    Alert.alert(
+      'Aviso',
+      `Você cancelou o agendamento com o ${response.data.provider.name}.`
+    );
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Title>Appointments</Title>
+
+        <List
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment data={item} onCancel={() => handleCancel(item.id)} />
+          )}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 Dashboard.navigationOptions = {
